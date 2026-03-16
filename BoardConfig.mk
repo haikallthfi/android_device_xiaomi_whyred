f5e95c8180fe68b18cb71cb8d9cba7ec574db500
haikallthfi <haikalluthfianinobaluqia598@gmail.com>
2026-03-17 05:43:01 +0700

    whyred: Add broken rules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,9 @@ DEVICE_PATH := device/xiaomi/whyred
 TARGET_BOOTLOADER_BOARD_NAME := sdm660
 TARGET_NO_BOOTLOADER := true
 
+# Broken Rules
+BUILD_BROKEN_DUP_RULES := true
+
 # Platform
 TARGET_BOARD_PLATFORM := sdm660
 
